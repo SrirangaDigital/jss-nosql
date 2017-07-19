5e8691fcd5ab00f5318c035142eb1d8164a64933
suresh07 <soori_rozee@yahoo.com>
2017-07-19 16:18:52 +0530

    bug fix in config

--- application/config/config.php
+++ application/config/config.php
@@ -2,8 +2,8 @@
 
 define('BASE_URL', 'http://localhost/jss-nosql/');
 define('PUBLIC_URL', BASE_URL . 'public/');
-define('DATA_URL', PUBLIC_URL . 'metaData/');
-define('METADATA_URL', PUBLIC_URL . 'data/');
+define('DATA_URL', PUBLIC_URL . 'data/');
+define('METADATA_URL', PUBLIC_URL . 'metaData/');
 define('JSON_PRECAST_URL', BASE_URL . 'json-precast/');
 define('FLAT_URL', BASE_URL . 'application/views/flat/');
 define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
@@ -11,8 +11,8 @@ define('STOCK_IMAGE_URL', PUBLIC_URL . 'images/stock/');
 // Physical location of resources
 define('PHY_BASE_URL', '/var/www/html/jss-nosql/');
 define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
-define('PHY_DATA_URL', PHY_PUBLIC_URL . 'metaData/');
-define('PHY_METADATA_URL', PHY_PUBLIC_URL . 'data/');
+define('PHY_DATA_URL', PHY_PUBLIC_URL . 'data/');
+define('PHY_METADATA_URL', PHY_PUBLIC_URL . 'metaData/');
 define('PHY_JSON_PRECAST_URL', PHY_BASE_URL . 'json-precast/');
 define('PHY_FLAT_URL', PHY_BASE_URL . 'application/views/flat/');
 define('PHY_STOCK_IMAGE_URL', PHY_PUBLIC_URL . 'images/stock/');
