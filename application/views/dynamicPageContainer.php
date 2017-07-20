93f30b9b9483855d16e994223a691088005940ea
suresh07 <soori_rozee@yahoo.com>
2017-07-20 13:32:06 +0530

    Project: nav bar links changed

--- application/views/dynamicPageContainer.php
+++ application/views/dynamicPageContainer.php
@@ -5,17 +5,8 @@
             <ul class="list-inline sub-nav">
                 <li><a href="<?=BASE_URL?>listing/categories/Letter">Letters</a></li>
                 <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/categories/Article">Articles</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/categories/Book">Books</a></li>
-                <li><a>·</a></li>
                 <li><a href="<?=BASE_URL?>listing/categories/Photograph">Photographs</a></li>
                 <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/categories/Artwork">Artworks</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/categories/Brochure">Brochures</a></li>
-                <li><a>·</a></li>
-                <li><a href="<?=BASE_URL?>listing/categories/Miscellaneous">Miscellaneous</a></li>
                 <li id="searchForm">
                     <form class="navbar-form" role="search" action="<?=BASE_URL?>search/field/" method="get">
                         <div class="input-group add-on">
