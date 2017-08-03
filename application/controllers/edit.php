909d095116441ad5307823471c9d1193b2ade682
shivasdst <shivably04sdst@gmail.com>
2017-08-03 11:59:59 +0530

    edit artefact updated and also added foreignkey method

--- application/controllers/edit.php
+++ application/controllers/edit.php
@@ -11,8 +11,33 @@ class edit extends Controller {
 	public function artefact($query, $id='') {
 	
 		$data = $this->model->editArtefact($id);
-		($data) ? $this->view('edit/artefact', $data) : $this->view('error/index');
+
+		if($data){
+			
+			$foreignKeys = $this->model->getForeignKeyTypes(FOREIGN_KEY_TYPE);
+			$data->foreignKeys =  (array)$foreignKeys;
+			$this->view('edit/artefact', $data); 
+		} else {
+			
+			$this->view('error/index');
+		}
+	}	
+
+	public function foreignkey($query, $key, $value) {
+	
+		$data = $this->model->editForeignKey($key,$value);
+
+		if($data){
+
+			$this->view('edit/foreignkey', $data); 
+		} else {
+			
+			$this->view('error/index');
+		}
 	}
+
+
+
 
 	public function updateArtefactJson() {
 		
@@ -31,28 +56,36 @@ class edit extends Controller {
 			$path = PHY_METADATA_URL . $fileContents['id'] . "/index.json";
 			$collectionName = ARTEFACT_COLLECTION;
 			$id = $fileContents['id'];
+			$idKey = 'id';
+			$url =  BASE_URL . 'describe/artefact/' . str_replace('/', '_', $id);
 		}
 		else{
 
 			$path = PHY_FOREIGN_KEYS_URL . $fileContents['ForeignKeyType'] . "/" . $fileContents['ForeignKeyId'] . ".json";
 			$collectionName = FOREIGN_KEY_COLLECTION;
 			$id = $fileContents['ForeignKeyId'];
+			$idKey = 'ForeignKeyId';
+			$url =  BASE_URL;
+
 		}
 
+		// var_dump($path);
+		// echo "<br /><br />";
+		// var_dump($collectionName);
+		// echo "<br /><br />";
 
 		$fileContentsJson = json_encode($fileContents,JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
 		if(file_put_contents($path, $fileContentsJson))
 		{
-			$this->model->syncArtefactJsonToDB($id, $collectionName);
+			$this->model->syncArtefactJsonToDB($idKey, $id, $collectionName, $path);
 			
 			if(REQUIRE_GIT_TRACKING)
 			{
 				$this->redirect('gitcvs/updateRepo/' . str_replace('/', '_', $id));
 			}
 			else
-			{
-				$url =  BASE_URL . 'describe/artefact/' . str_replace('/', '_', $id);
+			{				
 				$this->absoluteRedirect($url);
 			}
 			
