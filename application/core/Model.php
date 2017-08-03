954cbdc4e834e2dd9a46ddf803018434e210b879
shivasdst <shivably04sdst@gmail.com>
2017-08-03 12:01:49 +0530

    Core: sync artefact Json to DB changed

--- application/core/Model.php
+++ application/core/Model.php
@@ -55,19 +55,20 @@ class Model {
 		return ($firstLeaf) ? str_replace(PHY_DATA_URL, DATA_URL, $firstLeaf) : STOCK_IMAGE_URL . 'default-image.png';
 	}
 
-	public function syncArtefactJsonToDB($id, $collectionName){
+	public function syncArtefactJsonToDB($idKey, $id, $collectionName, $path){
 
 		$db = $this->db->useDB();
 		$collection = $this->db->selectCollection($db, $collectionName);
 
-		$jsonFile = PHY_METADATA_URL . $id . '/index.json';
+		// $jsonFile = PHY_METADATA_URL . $id . '/index.json';
+		$jsonFile = $path;
 
 		$contentString = file_get_contents($jsonFile);
 		$content = json_decode($contentString, true);
 		$content = $this->beforeDbUpadte($content);
 
 		$result = $collection->replaceOne(
-			[ 'id' => $id ],	
+			[ $idKey => $id ],	
 			$content
 		);
 
@@ -103,7 +104,7 @@ class Model {
 
 	public function unsetControlParams($data){
 
-		$controlParams = ['_id', 'ForeignKeyType', 'ForeignKeyId', 'AccessLevel'];
+		$controlParams = ['_id', 'AccessLevel','oid'];
 
 		foreach ($controlParams as $param) {
 
