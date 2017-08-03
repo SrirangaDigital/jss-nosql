b238b8bbfab2a2f91eab6a387fee25c0937239bb
shivasdst <shivably04sdst@gmail.com>
2017-08-03 12:02:35 +0530

    Core: foreign keys handled in this view

--- application/views/edit/artefact.php
+++ application/views/edit/artefact.php
@@ -3,16 +3,18 @@
         <div class="col-md-4">
             <div class="image-reduced-size">
                 <img class="img-responsive" src="<?=$data->thumbnailPath?>">
-                <?php $idURL = $data->idURL; 
+                <?php $idURL = $data->idURL;
+                      $foreignKeys = $data->foreignKeys;
+                      unset($data->foreignKeys);
                       unset($data->thumbnailPath); 
-                      unset($data->idURL); 
+                      unset($data->idURL);
                 ?>
             </div>
         </div>            
         <div class="col-md-8">
             <div class="image-desc-full">
                 <form  method="POST" class="form-inline updateDataArchive" role="form" id="updateData" action="<?=BASE_URL?>edit/updateArtefactJson" onsubmit="return validate()">
-                    <?=$viewHelper->displayDataInForm(json_encode($data))?>
+                    <?=$viewHelper->displayDataInForm(json_encode($data),$foreignKeys)?>
                 </form>    
             </div>
         </div>
